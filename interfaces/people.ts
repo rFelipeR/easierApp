a7d6ca096da48714b6dd7e0ff30d82d4b70dc495
rFelipeR <reckziegel.felipe@outlook.com>
2021-10-08 21:21:33 -0300

    Config: Some changes

--- interfaces/people.ts
+++ interfaces/people.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore/lite";
+import { addDoc, collection, getDocs } from "firebase/firestore/lite";
 import { db } from './../services/firebase'
 
 export default interface People {
@@ -12,6 +12,14 @@ export async function getPeople() {
     const snapshot = await getDocs(col);  //pega todos os dados da collection em questão, people;
     return snapshot;
 }
+
+
+// export async function addPeople(people: People) {
+//     const col = collection(db, 'people');
+//     // const docRef = await addDoc(col, people)
+//     // return addDoc(col, people)
+
+// }
 
 
 //collection = tabela
--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCXXyoInY2IlqGfCDQiGXRJzmn7jhZAW8s",
-  authDomain: "easierdb.firebaseapp.com",
-  projectId: "easierdb",
-  storageBucket: "easierdb.appspot.com",
-  messagingSenderId: "766745143648",
-  appId: "1:766745143648:web:17dbc0df64421c34d21605",
-  measurementId: "G-LRXPJK53Z9"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId:  process.env.measurementId
 };
