489d05a0310c7e67a168a07758f53fa62e358c1d
rFelipeR <reckziegel.felipe@outlook.com>
2021-11-09 21:56:42 -0300

    UPGRADE: Interface - some changes

--- interfaces/people.ts
+++ interfaces/people.ts
@@ -2,20 +2,21 @@ import { addDoc, collection, getDocs } from "firebase/firestore/lite";
 import { db } from './../services/firebase'
 
 export default interface People {
-    Id: string,
-    Name: string,
-    Document: string
+    nome: string,
+    documento: string,
+    email: string,
+    saldo: number
 }
 
 export async function getPeople() {
-    const col = collection(db, 'people');
+    const col = collection(db, 'pessoa');
     const snapshot = await getDocs(col);  //pega todos os dados da collection em questão, people;
     return snapshot;
 }
 
 
 export async function addPeople(people: People) {
-    const col = collection(db, 'people');
+    const col = collection(db, 'pessoa');
     // const docRef = await addDoc(col, people)
     return addDoc(col, people)
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
+import { Tooltip, Avatar } from 'antd'
 import { QuerySnapshot } from 'firebase/firestore/lite'
 import type { NextPage } from 'next'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import People, { addPeople, getPeople } from '../interfaces/people'
+import Card from '../interfaces/people'
 import styles from '../styles/Home.module.css'
+import { AntDesignOutlined, UserOutlined } from "@ant-design/icons";
 
 const Home: NextPage = () => {
   const [list, setList] = useState<QuerySnapshot>();
   const [newValue, setNewValue] = useState('');
+  const [cartao, setCartao] = useState<Card>();
 
   useEffect(() => {
     getPeople().then(x =>
@@ -21,25 +25,42 @@ const Home: NextPage = () => {
       .catch(error => alert(console.log(error)))
   }
 
-
   function handleAdd() {
-    const newPeople = { Name: newValue } as People
+    const newPeople = { nome: newValue } as People
     addPeople(newPeople).then(x => {
       alert('Salvo!')
       handleGetPeople()
     }).catch(error => console.log(error));
   }
 
+  useEffect(() => { console.log(cartao) }, [cartao])
+
   return (
     <div className={styles.container}>
-      <div style={{margin:'15px 0'}}>
-      <input value={newValue} onChange={(e) => setNewValue(e.target.value)} />
-      <button onClick={handleAdd}>+ Add</button>
+      <Avatar.Group style={{ position: 'absolute', top: '50%', left: '10%' }}>
+        <Avatar src="https://joeschmoe.io/api/v1/random" />
+        <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+        <Tooltip title="Ant User" placement="top">
+          <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+        </Tooltip>
+        <Avatar style={{ backgroundColor: '#1890ff' }} icon={<AntDesignOutlined />} />
+      </Avatar.Group>
+      <div style={{ margin: '15px 0' }}>
+        <input value={newValue} onChange={(e) => setNewValue(e.target.value)} />
+        <button onClick={handleAdd}>+ Add</button>
       </div>
       Listagem
       {list?.docs?.map(doc => {
         const people = doc.data() as People;
-        return <span>{people.Id} – {people.Name}</span>
+        return (
+          <span>
+            {people.documento} – {people.nome}
+            <button onClick={() => {
+              const card = ({ nome: people.nome,  documento: people.documento } as Card)
+              setCartao(card)
+            }}> Selecionar</button>
+          </span>
+        )
       })}
 
       <footer className={styles.footer}></footer>
